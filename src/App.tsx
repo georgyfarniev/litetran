0c775bbd7bf3ca043ac67ba58f82f0bf5aa8a4fc
Georgy Farniev <gfarniev@icloud.com>
2020-05-13 21:01:04 +0400

    implement translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
-
-const { ipcRenderer } = window.require("electron")
-
-function Header(props: any) {
-  return (
-    <p onClick={props.onClicked}>Suppa puppa header!111 {props.foo}</p>
-  )
-}
-
-function useSelection() {
-  const [selected, setSelected] = useState('')
-
-  useEffect(() => {
-    const f = (_: any, msg: string) => setSelected(msg)
-    ipcRenderer.on('selection', f)
-    return () => { ipcRenderer.off('selection', f) }
-  })
-
-  return selected
-}
+import { useSelection, useTranslate } from './hooks'
 
 function App() {
   const selected = useSelection()
   const [fromLang, setFromLang] = useState('en')
   const [toLang, setToLang] = useState('ru')
+
+  const { result, loading } = useTranslate({
+    from: 'en',
+    to: 'ru',
+    text: selected
+  })
 
   const options: any[] = [
     { key: 'en', text: 'English' },
@@ -37,13 +23,13 @@ function App() {
 
   return (
     <div className="lt-app">
-      <TextField multiline value={selected}/>
+      <TextField multiline value={selected} />
       <div className="lt-toolbar">
         <Dropdown
-        className="lt-toolbar-select"
+          className="lt-toolbar-select"
           placeholder="Select an option"
           options={options}
-          selectedKeys={[ fromLang ]}
+          selectedKeys={[fromLang]}
           onChange={(ev: any) => console.dir(ev.target.value)}
         />
         <DefaultButton
@@ -54,17 +40,16 @@ function App() {
           className="lt-toolbar-select"
           placeholder="Select an option"
           options={options}
-          selectedKeys={[ toLang ]}
+          selectedKeys={[toLang]}
         />
         <PrimaryButton
           className="lt-toolbar-btn"
           text="Translate"
         />
-       
       </div>
-        <p>From: {fromLang}</p>
-        <p>to: {toLang}</p>
-        <TextField multiline />
+      <p>From: {fromLang}</p>
+      <p>to: {toLang}</p>
+      <TextField multiline value={result} />
     </div>
   )
 
---
+++ src/hooks.ts
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+import useAxios from '@use-hooks/axios'
+
+const { ipcRenderer } = window.require("electron")
+
+export function useSelection() {
+  const [selected, setSelected] = useState('')
+
+  useEffect(() => {
+    const f = (_: any, msg: string) => setSelected(msg)
+    ipcRenderer.on('selection', f)
+    return () => { ipcRenderer.off('selection', f) }
+  })
+
+  return selected
+}
+
+const TRANSLATE_API = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+const KEY = 'trnsl.1.1.20200513T162329Z.67f4bd4475e32475.a1863f6d5af5320919b4bab95fc2e78c709d5748'
+
+export interface IUseTranslateOpts {
+  from: string
+  to: string
+  text: string
+}
+
+export interface IUseTranslateResponse {
+  result: string
+  loading: boolean
+  error: boolean
+}
+
+export function useTranslate(opts: IUseTranslateOpts): IUseTranslateResponse {
+  const { response, loading, error } = useAxios({
+    url: TRANSLATE_API,
+    method: 'GET',
+    options: {
+      params: {
+        key: KEY,
+        text: opts.text,
+        lang: `${opts.from}-${opts.to}`
+      }
+    },
+    trigger: opts.text
+  })
+
+  return {
+    result: response?.data?.text?.[0],
+    loading,
+    error: !!error
+  }
+}
